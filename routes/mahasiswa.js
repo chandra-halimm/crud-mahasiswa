83f7c17cbb15f1588267b309bb0b787afb12ae4f
Chandra Halim <chan.halim98@gmail.com>
2023-04-14 15:07:01 -0700

    f

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mahasiswaModels = require("../models/mahasiswa");
+const { where } = require("sequelize");
 
 router.get("/", async (req, res) => {
   try {
@@ -19,11 +20,96 @@ router.get("/", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: "server error",
+      message: "get methode internal server error",
     });
   }
 });
 
-router.post;
+router.post("/", async (req, res) => {
+  try {
+    const { nim, name, email, alamat } = req.body;
+    const userMahasiswa = await mahasiswaModels.create({
+      nim,
+      name,
+      email,
+      alamat,
+    });
+
+    if (!userMahasiswa) {
+      res.status(400).json({
+        message: "failed input",
+      });
+    } else {
+      res.status(200).json({
+        data: userMahasiswa,
+        message: "input success",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "create methode internal server error",
+    });
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const { nim, name, email, alamat, nimBaru } = req.body;
+    const userMahasiswa = await mahasiswaModels.update(
+      {
+        nim: nimBaru,
+        name,
+        email,
+        alamat,
+      },
+      {
+        where: { nim: nim },
+      }
+    );
+
+    if (!userMahasiswa) {
+      res.status(400).json({
+        message: "update failed",
+      });
+    } else {
+      res.status(200).json({
+        data: userMahasiswa,
+        message: "update Berhasil",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "update methode internal server error",
+    });
+  }
+});
+
+router.delete("/:nim", async (req, res) => {
+  try {
+    const { nim } = req.params;
+    const userMahasiswa = await mahasiswaModels.findOne({
+      where: { nim: nim },
+    });
+
+    if (!userMahasiswa) {
+      res.status(400).json({
+        message: "deleted fail",
+      });
+    } else {
+      await userMahasiswa.destroy();
+      res.status(200).json({
+        data: userMahasiswa,
+        message: "deleted success",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "delete methode internal server error",
+    });
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const sequelize = require("./db.config");
 
+app.use(express.json());
+
 sequelize.sync().then(() => {
   console.log("database ready");
 });
